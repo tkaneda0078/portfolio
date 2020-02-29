7ba43cddcabe25c27d2a5610006463a85a9bf869
金田尚大 <1111510078r21@gmail.com>
2020-02-29 13:25:20 +0900

    webpack bootstrap追加

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+
+import '../css/global.scss'
+
+import $ from 'jquery';
+require('bootstrap');
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var Encore = require('@symfony/webpack-encore')
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+  Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
+}
+
+Encore
+  .setOutputPath('public/build/')
+  .setPublicPath('/build')
+  .addEntry('app', './assets/js/app.js')
+
+  .splitEntryChunks()
+
+  .enableSingleRuntimeChunk()
+
+  .cleanupOutputBeforeBuild()
+  .enableBuildNotifications()
+  .enableSourceMaps(!Encore.isProduction())
+  // enables hashed filenames (e.g. app.abc123.css)
+  .enableVersioning(Encore.isProduction())
+
+  // enables @babel/preset-env polyfills
+  .configureBabelPresetEnv((config) => {
+    config.useBuiltIns = 'usage'
+    config.corejs = 3
+  })
+
+  // enables Sass/SCSS support
+  .enableSassLoader()
+
+
+module.exports = Encore.getWebpackConfig()
