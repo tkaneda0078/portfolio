dba439b8cd7acd778b455c0cd4f3e6921d69160f
金田尚大 <1111510078r21@gmail.com>
2020-03-01 11:36:48 +0900

    blog content

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,6 +8,9 @@ class HomeController extends AbstractController
 {
   public function indexAction()
   {
-    return $this->render('Home/index.html.twig', []);
+    // TODO blogコンテンツをcsmから取得する
+    $views['blogs'] = [['title' => 'title1', 'text' => 'text1'], ['title' => 'title2', 'text' => 'text2']];
+
+    return $this->render('Home/index.html.twig', $views);
   }
 }
